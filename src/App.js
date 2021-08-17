a70379895f0539b1c933e287600e2a22ab352e78
ndrsonya <altusar.sonya@gmail.com>
2021-08-17 22:21:12 +0300

    started to work on the task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import rawText from './bookText.txt';
+
+console.log(rawText);
 
 function App() {
+
+  let decodedText;
+  fetch(rawText)
+    .then(r => r.text())
+    .then(text => {
+      //console.log('text decoded:', text);
+      text = text
+        .replace(/\n\r/g, "\n")
+        .replace(/\r/g, "\n")
+        .split(/\n{2,}/g);
+    //  console.log(text); //3257
+      const distinctContent = [...new Set(text)]
+      //filter to have elements that starts with number
+      const result = distinctContent.filter(word => word.length > 6);
+      distinctContent.map(el => console.log(el))
+     //console.log(distinctContent); //3254
+    });
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
