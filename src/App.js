cce31822ad1e8994a263dd0e3cbcfaad5d4e6b54
ndrsonya <altusar.sonya@gmail.com>
2021-08-19 20:45:32 +0300

    some styles added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import logo from './logo.svg';
 import './App.css';
 import rulebookService from './services/rulebookContent'
 import gettersService from './services/getters'
-import ChaptersList from './components/Chapters';
 import RulesList from './components/Rules';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
-  Redirect,
-  useRouteMatch,
-  useHistory,
+  Link
 } from "react-router-dom"
 
 function App() {
   const [chapters, setChapters] = useState([]);
-  const [selectedChapter, setSelectedChapter] = useState();
+
 
   useEffect(() => {
     rulebookService.getRulebookContent().then(res => {
@@ -29,30 +24,20 @@ function App() {
   console.log("chapter state")
   console.log(chapters)
 
-  const handleClick = (index) => {
-    setSelectedChapter(index);
-    console.log("I WORK")
-    console.log(index)
-  }
 
   return (
-    <Router>
 
-      <div>
-        <Link to="/">home</Link>
-        <br />
-        <Link to={`${notes}`}>notes</Link>
-        <br />
-        <Link to="/users">users</Link>
+    <div className="App" >
+      <div className="LeftPart">
+        <Link className="Link" to="/">home</Link>=
+        <Link className="Link" to={`${notes}`}>notes</Link>
+
 
         {
           chapters.map((el, index) => {
             return (
               <div key={index}>
-
-                <Link
-                  to={`${el.chapterIndex}`}
-                  onClick={() => handleClick(el.chapterIndex)}
+                <Link className="Link" to={`${el.chapterIndex}`}
                 >
                   {el.chapterIndex} {el.chapterDescription}
                 </Link>
@@ -70,25 +55,23 @@ function App() {
         </Route>
         {
           chapters.map((el, index) => {
-            
+
             return (
               <Route key={index} path={`/${el.chapterIndex}`}>
-                <RulesList data = {el.rules}/>
+                <div className="RightPart">
+                  <RulesList data={el.rules} />
+                </div>
               </Route>
             )
           })
 
         }
-        
+
         <Route path="/">
           <h1>home</h1>
         </Route>
       </Switch>
-
-      <div>
-        <i>Note app, Department of Computer Science 2021</i>
-      </div>
-    </Router>
+    </div>
   );
 }
 
